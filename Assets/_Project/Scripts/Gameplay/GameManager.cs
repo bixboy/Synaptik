1509b8e026618d3b38c6cb3a5e3ff97dbe6a0461
bixboy <pierrelamare62@gmail.com>
2025-10-06 14:38:01 +0200

    [ADD] Manager

---
+++ Assets/_Project/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Mission
+{
+    public string Name;
+    public bool IsFinish;
+    public IInteraction UfoRef;
+}
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] 
+    private List<Mission> UfoList { get; set; } = new List<Mission>();
+    
+    private float Mistrust { get; set; } = 0f;
+    private float MaxMistrust = 0f;
+
+    public void ClearAll()
+    {
+        UfoList.Clear();
+        Mistrust = 0f;
+    }
+    
+    public bool RegisterMission(Mission mission)
+    {
+        foreach (var ufo in UfoList)
+        {
+            if (ufo.Name == mission.Name)
+            {
+                print("Mission already registered");
+                return false;   
+            }
+        }
+        
+        UfoList.Add(mission);
+        return true;
+    }
+
+    public void SetMissionFinish(string missionName)
+    {
+        for (int i = 0; i < UfoList.Count; i++)
+        {
+            if (UfoList[i].Name == missionName)
+            {
+                var ufo = UfoList[i];
+                
+                ufo.IsFinish = true;
+                UfoList[i] = ufo;
+            }
+        }
+    }
+
+    public void SetMistrustValue(float value)
+    {
+        Mistrust = value;
+
+        if (Mistrust >= MaxMistrust)
+        {
+            print("Defeat");
+        }
+    }
+}
