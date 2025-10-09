299acccd35294d621314b1243ee8cb7d3b37c94f
bixboy <pierrelamare62@gmail.com>
2025-10-09 11:45:43 +0200

    [MOD] Loading screen feedback

--- Assets/_Project/Scripts/UI/LoadingScreenManager.cs
+++ Assets/_Project/Scripts/UI/LoadingScreenManager.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework;
 
 public class LoadingScreenManager : MonoBehaviour
 {
     public static LoadingScreenManager Instance;
 
     [Header("Références")]
+    private LoadingText loadingText;
     [SerializeField] private GameObject loadingScreenPrefab;
 
     [Header("Paramètres")]
     [SerializeField] private float fadeDuration = 1f;
     [SerializeField] private float holdDurationBeforeLoad = 0.5f;
     [SerializeField] private float holdDurationAfterSceneLoaded = 0.5f;
-    
+
     [Header("Text")]
     public List<string> TextFiller = new List<string>();
 
@@ -45,6 +45,7 @@ public class LoadingScreenManager : MonoBehaviour
         currentLoadingInstance = Instantiate(loadingScreenPrefab);
         DontDestroyOnLoad(currentLoadingInstance);
         currentCanvasGroup = currentLoadingInstance.GetComponentInChildren<CanvasGroup>();
+        loadingText = currentLoadingInstance.GetComponentInChildren<LoadingText>();
 
         if (!currentCanvasGroup)
         {
@@ -54,9 +55,13 @@ public class LoadingScreenManager : MonoBehaviour
 
         currentCanvasGroup.alpha = 0f;
 
+        if (loadingText)
+            loadingText.StartText(TextFiller);
+
+        float estimatedTotalDuration = fadeDuration * 2 + holdDurationBeforeLoad + holdDurationAfterSceneLoaded;
+        StartCoroutine(UpdateLoadingProgress(estimatedTotalDuration));
+
         yield return StartCoroutine(Fade(0f, 1f));
-        
-        
 
         yield return new WaitForSeconds(holdDurationBeforeLoad);
 
@@ -65,16 +70,19 @@ public class LoadingScreenManager : MonoBehaviour
 
         while (asyncLoad.progress < 0.9f)
         {
+            if (loadingText)
+                loadingText.Loading(asyncLoad.progress);
+            
             yield return null;
         }
 
         asyncLoad.allowSceneActivation = true;
 
-        yield return new WaitUntil(() => SceneManager.GetActiveScene().name == sceneName);
+       yield return new WaitUntil(() => SceneManager.GetActiveScene().name == sceneName);
 
         yield return new WaitForSeconds(holdDurationAfterSceneLoaded);
 
-        yield return StartCoroutine(Fade(1f, 0f));
+       yield return StartCoroutine(Fade(1f, 0f));
 
         Destroy(currentLoadingInstance);
         currentLoadingInstance = null;
@@ -94,5 +102,25 @@ public class LoadingScreenManager : MonoBehaviour
 
         if (currentCanvasGroup)
             currentCanvasGroup.alpha = to;
+    }
+
+    private IEnumerator UpdateLoadingProgress(float duration)
+    {
+        if (!loadingText) 
+            yield break;
+
+        duration *= 0.9f;
+
+        float time = 0f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float progress = Mathf.Clamp01(time / duration);
+            loadingText.Loading(progress);
+            
+            yield return null;
+        }
+
+        loadingText.Loading(1f);
     }
 }
